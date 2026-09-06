e6a8eb6c6930f3ceaae3486c03d5433f34fe1cb8
Ian Arsenault <161642785+ianarsenault-tn@users.noreply.github.com>
2026-09-06 11:24:57 -0700

    Export requestStaffReset from app-views barrel

--- src/app-views.js
+++ src/app-views.js
@@ -1,2 +1,2 @@
 export { renderCase, renderLogin, renderSwapSheet, renderAddSheet } from "./app-views-case.js";
-export { renderManager, render } from "./app-views-mgr.js";
+export { renderManager, render, requestStaffReset } from "./app-views-mgr.js";
